6aa377be2f93d90eaa1da00f7db42717f12466dd
Benjamin P <benpodellsmu@gmail.com>
2019-06-21 11:12:28 -0500

    Another Push

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,13 +3,19 @@ import os
 import csv
 
 #Creates Path to budget_data CSV File
-budget_data = 'C:/Users/btech/Desktop/python-challenge/PyBank/Resources/budget_data.csv'
+#Had trouble opening 'budget_data.csv so implemented os.getcwd() to help open filepath
+#https://www.tutorialspoint.com/python/os_getcwd.htm
+
+print(os.getcwd())
+budget_data = os.path.join('PyBank', 'Resources', 'budget_data.csv')
 
 #Allows the File Path to be Read based on CSV Conditions
 with open(budget_data, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    
     #Reads the Header of budget_data CSV
     csv_header = next(csvreader)
+    
     #Reads the First Row of budget_data CSV
     row_one = next(csvreader)
 
@@ -19,46 +25,70 @@ with open(budget_data, newline = "") as csvfile:
     holder_count = 0
     adjust_count = 0
     
+    #Stores profit and date data 
     profit_list = []
     date_list = []
     
+    #Totals the months, profitloss count, and adds to the holder count for the very first row before going through the other rows
     month_count = month_count + 1
     profitloss_count = profitloss_count + int(row_one[1])
     holder_count = int(row_one[1])
 
+    #creates for loop reading each row in csv reader
     for row in csvreader:
+        
+        #Goes through the dates holder and tracks changes
         date_list.append(row[0])
+        
+        #Adjusts the profits holder by iterating through each row and then moving to next holder_count which assists with pushing through the rows
         adjust_count = int(row[1]) - holder_count
         profit_list.append(adjust_count)
         holder_count = int(row[1])
+        
+        #Calculates the total months exluding header and first row
         month_count = month_count + 1
+        
+        #Calculates them total profit loss count excluding header and first row
         profitloss_count = profitloss_count + int(row[1])
 
+    #Finds the Greatest Profit Increase's Value and Date by indexing through profit list and returning the maximum value and date using 'max()' function
     highest_increase = max(profit_list)
     highest_index  = profit_list.index(highest_increase)
     highest_date = date_list[highest_index]
 
+    #Finds the Greatest Profit Decreases's Value and Date by indexing through profit list and returning the minimum value and date using 'min()' function
     lowest_decrease = min(profit_list)
     lowest_index = profit_list.index(lowest_decrease)
     lowest_date = date_list[lowest_index]
 
+    #Formula that Calulates the Average Change in Profit by dividing the sum of all profits in created profit holder by the entire length of profit holder 
     average = sum(profit_list) / len(profit_list)
 
+    #Printing and Summarizing Data 
     print("Financial Analysis")
-    print("---------------------")
+    print("------------------")
     print(f'Total Months: {str(month_count)}')
     print(f'Total: ${str(profitloss_count)}')
+    
+    #Round function that rounds the average change to 2 decimal places
     print(f'Average Change: $ {str(round(average,2))}')
     print(f'Greatest Increase in Profits: {highest_date} (${str(highest_increase)})')
     print(f'Greatest Decrease in Profits: {lowest_date} (${str(lowest_decrease)})')
 
-    output = open("output.txt", "w")
+    #Creates and exports an 'output' text file that is write only
+    export_txtfile = open("output.txt", "w")
 
+    #Creates the output text files lines 'l1, l2, ect...' based on Printing and Summarizing Data
     l1 = "Financial Analysis"
-    l2 = "---------------------"
+    l2 = "------------------"
     l3 = str(f'Total Months: {str(month_count)}')
     l4 = str(f'Total: ${str(profitloss_count)}')
+    
+    #Round function that rounds the average change to 2 decimal places
     l5 = str(f'Average Change: $ {str(round(average,2))}')
     l6 = str(f'Greatest Increase in Profits: {highest_date} (${str(highest_increase)})')
     l7 = str(f'Greatest Decrease in Profits: {lowest_date} (${str(lowest_decrease)})')
-    output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(l1,l2,l3,l4,l5,l6,l7))
+    
+    #Syntax that outputs each line of text file for all 7 lines
+    #https://stackoverflow.com/questions/21019942/write-multiple-lines-in-a-file-in-python/21020007
+    export_txtfile.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(l1,l2,l3,l4,l5,l6,l7))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,24 +3,33 @@ import os
 import csv
 
 #Creates Path to election_data CSV File
-election_data = 'C:/Users/btech/Desktop/python-challenge/PyPoll/Resources/election_data.csv'
+#Had trouble opening 'budget_data.csv so implemented os.getcwd() to help open filepath
+#https://www.tutorialspoint.com/python/os_getcwd.htm
+print(os.getcwd())
+election_data = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 
 #Allows the File Path to be Read based on CSV Conditions
 with open(election_data, newline = "") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    
     #Reads the Header of election_data CSV
     csv_header = next(csvreader)
 
     #Setting Total Votes Count to Zero In Order To Track Addition Towards Total 
     vote_counter = 0
     
+    #Stores Candidate, Voter, and Percent data
     candidate_list = []
     vote_list = []
     percent_list = []
 
+    #creates for loop reading each row in csv reader
     for row in csvreader:
+        
+        #Calculates the Total Vote Count
         vote_counter = vote_counter + 1
 
+        #Conditional that checks if while iterating the candidate is not in Third Column, adds them to created Candidate Holder (candidate_list) then indexes them in Candidate Holder or else will continue to add/append their votes to vote_list
         if row[2] not in candidate_list:
             candidate_list.append(row[2])
             find_candidate = candidate_list.index(row[2])
@@ -29,40 +38,53 @@ with open(election_data, newline = "") as csvfile:
             find_candidate = candidate_list.index(row[2])
             vote_list[find_candidate] = vote_list[find_candidate] + 1
 
+    #Creates for loop that calculates Vote Percentage
     for v in vote_list:
-        percent = (v/vote_counter) * 100
-        percent = round(percent)
-        percent = "%.3f%%" % percent
+        #Format that floats Percentage Formula to 3 decimal places i.e. '3f' and adds to the Percent Holder (percent_list)
+        #https://codereview.stackexchange.com/questions/69542/function-to-return-percentages-in-python
+        percent = '{0:.3f}'.format((v / vote_counter * 100))
         percent_list.append(percent)
 
+    #Finds Winning Candidate's highest Voting Count from the Created Vote Holder (vote_list) Value by indexing through (vote_list), returning the maximum value using 'max()' function, and storing it in variable called winner
     Win = max(vote_list)
     find_candidate = vote_list.index(Win)
     winner = candidate_list[find_candidate]
 
+#Printing and Summarizing Data 
 print("Election Results!")
 print("----------------")
 print(f'Total Votes: {str(vote_counter)}')
 print("--------------------")
+
+#Creates For loop that outputs candidates name, percent of votes, and number of votes from each of the created holders by going through the range of each lists length 
 for x in range(len(candidate_list)):
-    print(f'{candidate_list[x]}: {str(percent_list[x])} ({str(vote_list[x])})')
+    print(f'{candidate_list[x]}: {str(percent_list[x])}% ({str(vote_list[x])})')
 print("-------------------------")
 print(f'Winner: {winner}')
 print("------------")
 
-output = open("output.txt", "w")
+#Creates and exports an 'output' text file that is write only
+export_txtfile = open("output.txt", "w")
 l1 = str("Election Results!")
 l2 = str("----------------")
 l3 = str(f'Total Votes: {str(vote_counter)}')
 l4 = str("--------------------")
-#Outputs file and creates new line, I referenced 
-#https://stackoverflow.com/questions/28462164/python-writing-to-a-new-line
-output.write('{}\n{}\n{}\n{}\n'.format(l1, l2, l3, l4))
+
+#Syntax that outputs each line of text file for l1, l2, l3,l4
+#https://stackoverflow.com/questions/21019942/write-multiple-lines-in-a-file-in-python/21020007
+export_txtfile.write('{}\n{}\n{}\n{}\n'.format(l1, l2, l3, l4))
+
+#Creates For loop that outputs candidates name, percent of votes, and number of votes from each of the created holders by going through the range of each lists length 
 for y in range(len(candidate_list)):
-    ln = str(f'{candidate_list[y]}: {str(percent_list[y])} ({str(vote_list[y])})')
-    output.write('{}\n'.format(ln))
+    ln = str(f'{candidate_list[y]}: {str(percent_list[y])}% ({str(vote_list[y])})')
+    
+    #Syntax that outputs each line of text file for ln
+    #https://stackoverflow.com/questions/21019942/write-multiple-lines-in-a-file-in-python/21020007
+    export_txtfile.write('{}\n'.format(ln))
+
 l5 = str("-------------------------")
 l6 = str(f'Winner: {winner}')
 l7 = str("------------")
-#Outputs file and creates new line, I referenced 
-#https://stackoverflow.com/questions/28462164/python-writing-to-a-new-line
-output.write('{}\n{}\n{}\n'.format(l5, l6, l7))
+#Syntax that outputs each line of text file for l5, l6, l7
+#https://stackoverflow.com/questions/21019942/write-multiple-lines-in-a-file-in-python/21020007
+export_txtfile.write('{}\n{}\n{}\n'.format(l5, l6, l7))
